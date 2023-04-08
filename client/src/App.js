90a00afc51530c0092b30f391e7502569a783687
RIhab212 <r.essabaouni@mundiapolis.ma>
2023-04-08 16:18:23 +0200

    wrbsite

--- client/src/App.js
+++ client/src/App.js
@@ -17,13 +17,13 @@ function App() {
     <BrowserRouter>
       <Routes>
           <Route path="/Jioujib" exact element={<Login/>} />
-          <Route path="/Signup" element={< Signup/>} />
-          <Route path="/AdminInterface" element={<AdminInterface/>}/>
-          <Route path="/UserLoggedInDetails" element={<UserLoggedInDetails/>}/>
-          <Route path="/Validrecords" element={<Validrecords/>}/>
+          <Route path="/signup" element={< Signup/>} />
+          <Route path="/adminInterface" element={<AdminInterface/>}/>
+          <Route path="/userLoggedInDetails" element={<userLoggedInDetails/>}/>
+          <Route path="/validrecords" element={<Validrecords/>}/>
           <Route path="/FormC" element={<FormC/>}/>
           <Route path="/Phonesignup" element={<Phonesignup/>}/>
-          <Route path="/Productsadmin" element={<Productsadmin/>}/>
+          <Route path="/productsadmin" element={<Productsadmin/>}/>
           <Route path="/OrderA" element={<OrderA/>}/>
       </Routes>
     </BrowserRouter>
