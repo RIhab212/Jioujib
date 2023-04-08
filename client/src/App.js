8d23f46e28b8c8c88d66fd895dea8a990fb97dad
RIhab212 <80919317+RIhab212@users.noreply.github.com>
2023-04-09 01:08:03 +0200

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -14,7 +14,7 @@ import "./App.css";
 
 function App() {
   return (
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <BrowserRouter>
       <Routes>
           <Route path="/Jioujib/" exact element={<Login/>} />
           <Route path="/Jioujib/signup" element={< Signup/>} />
