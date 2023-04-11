34cc5c05c4c1af49964270f3c964aecc9c670ee3
RIhab212 <r.essabaouni@mundiapolis.ma>
2023-04-11 03:09:43 +0200

    wrbsite

--- server/server.js
+++ server/server.js
@@ -192,8 +192,11 @@ io.on("connection", (socket) => {
   });
 
 });
-server.listen(process.env.PORT || 3001, () => { console.log("SERVER IS RUNNING"); });
-const port = process.env.PORT || 8080;
+server.listen(3001, () => {
+  console.log("SERVER IS RUNNING");
+});
+
+const port = process.env.PORT || 10000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
 
 
