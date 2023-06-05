700aeea7bcadee4d4842ac3d43f0898783c2befd
RIhab212 <r.essabaouni@mundiapolis.ma>
2023-06-05 23:21:19 +0200

    wrbsite

--- server/USer.js
+++ server/USer.js
@@ -1,18 +1,32 @@
 const express = require("express");
 const router = express.Router();
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 
 const User = mongoose.model("UserInfo");
 
+
 router.get("/", async (req, res) => {
+    const { fname, lname, email, password, gender } = req.body;
+
+    const encryptedPassword = await bcrypt.hash(password, 10);
     try {
-        const users = await User.find();
+        const user = await User.findOne({ email: req.body.email });
+        if (user) {
+            return res.status(400).json({ error: "User already exists" });
+        }
+        await User.create({
+            fname,
+            lname,
+            email,
+            password: encryptedPassword,
+            gender,
+        });
         userCount++;
-        res.status(200).json({ users });
-
+        res.status(200).json({ message: "User created successfully" });
     } catch (error) {
-        res.status(500).json({ error: "Une erreur s'est produite lors de la récupération des utilisateurs" });
+        res.status(500).json({ error: "An error occurred while creating user" });
     }
-});
 
+});
 module.exports = router;
