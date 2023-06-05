2c3e8fd048a7d051dd94b51b1d9b23d0ed7d1101
RIhab212 <r.essabaouni@mundiapolis.ma>
2023-06-06 00:23:40 +0200

    wrbsite

--- server/USer.js
+++ server/USer.js
@@ -30,7 +30,6 @@ const User = mongoose.model("UserInfo");
 router.get("/", async (req, res) => {
     try {
         const users = await User.find();
-        userCount++;
         res.status(200).json({ users });
     } catch (error) {
         res.status(500).json({ error: "Une erreur s'est produite lors de la récupération des utilisateurs" });
