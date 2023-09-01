396394432c87e1f4de6c248c5679b8633c3ee986
RIhab212 <r.essabaouni@mundiapolis.ma>
2023-09-01 03:08:00 +0200

    wrbsite

--- server/Products.js
+++ server/Products.js
@@ -19,7 +19,7 @@ const userSchema = new mongoose.Schema({
 	photo: { type: String, required: true },
 	isConfirmed: { type: Boolean, default: false },
 	Notif : { type: Boolean, default: true },
-	userId : {type:Number , required: true},
+	userId : {type:String , required: true},
 	status : { type: String, default: null},
 	createdAt: {
 		type: Date,
